94212b27f20ee28513c6a05cbb266287c5ea743c
marcin <marcin.kasinski@gmail.com>
2018-11-05 15:59:14 +0100

    remove filebeat start

--- docker/start.sh
+++ docker/start.sh
@@ -2,5 +2,5 @@
 
 #ls -l /
 #cat /etc/init.d/filebeat
-/etc/init.d/filebeat start
+#/etc/init.d/filebeat start
 java -jar -Dspring.profiles.active=$SPRING_PROFILE SpringBootRabbitMQListener-0.0.1-SNAPSHOT.jar
